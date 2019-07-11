72404a3a9bc67bb48946e931fd6d411657945643
김인혁 <kih2320@gmail.com>
2019-07-11 16:57:57 +0900

    16:57 commit

--- webapp/controller/Fileupload.controller.js
+++ webapp/controller/Fileupload.controller.js
@@ -24,20 +24,6 @@ sap.ui.define([
                     reader.onload = function(e){
                         var strCSV = e.target.result;
                         str = strCSV
-                        // var testCsv = e.target.result;
-                        // var arrCsv = testCsv.split(/\r?\n|\r/);
-                        // var title = 4;
-                        // var testData = [];
-                        // arrCsv.map(function(data,i){
-                        //     let row = data.split(',')
-                        //     row.map(function(row, row_index){
-                        //         console.log(row)
-                        //     })
-                        //     // data.split(',').map(function(zz,index){
-                        //     //     console.log(zz)
-                        //     // })
-                        // })
-                        // console.log(testData)
                         var arrCSV = strCSV.match(/[\w|\- .]+(?=,?)/g);
                         var noOfCols = 4;
                         var hdrRow = arrCSV.splice(0, noOfCols);
@@ -58,6 +44,32 @@ sap.ui.define([
                         oTable.setModel(oModel)
                     };
                     reader.readAsBinaryString(file)
+                }else {
+                    var reader = new FileReader();
+                    reader.onload = function(e){
+                        var strCSV = e.target.result;
+                        str = strCSV
+                        var arrCSV = strCSV.match(/[\w|\- .]+(?=,?)/g);
+                        var noOfCols = 4;
+                        var hdrRow = arrCSV.splice(0, noOfCols);
+                        // console.log(hdrRow)
+                        var data = [];
+                        while (arrCSV.length > 0){
+                            var obj = {};
+                            var row = arrCSV.splice(0, noOfCols);
+                            // console.log(row)
+                            for (var i = 0; i < row.length; i++){
+                                obj[hdrRow[i]] = row[i].trim();
+                            }
+                            data.push(obj)
+                        }
+                        console.log(data)
+                        var oModel = new sap.ui.model.json.JSONModel();
+                        oModel.setData(data);
+                        oTable.setModel(oModel)
+                    };
+                    reader.readAsBinaryString(blob)
+
                 }
             }
         },
